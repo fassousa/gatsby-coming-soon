cf0933f549831c91f48c44ad86a64e95a301a415
Fagnner <fagnnersousa@yahoo.com.br>
2019-05-31 12:21:17 -0300

    Change background image for desktop

--- src/components/image.js
+++ src/components/image.js
@@ -17,9 +17,9 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(relativePath: { eq: "desktop-soon.png" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid(maxWidth: 1600) {
               ...GatsbyImageSharpFluid
             }
           }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
 import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <div
+    className="background"
+    style={{
+      // Defaults are overwrite-able by setting one or each of the following:
+      backgroundSize: "contain",
+      backgroundPosition: "center",
+      backgroundRepeat: "cover",
+    }}
+  >
+    <Image />
+  </div>
 )
 
 export default IndexPage
